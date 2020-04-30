91dfbd0639da0469060845af21f2361696ee7dbe
Andrew MacLeod <A6macleod@gmail.com>
2020-04-30 17:21:58 -0400

    place ships vertically, no boundry

--- src/place_ships.js
+++ src/place_ships.js
@@ -1,15 +1,29 @@
+const rowList = ['a','b','c','d','e','f','g','h','i','j'];
+
 function newStartingPoint () {
   // find a random letter a-j
   // find a random number 1-10
-  const rowList = ['a','b','c','d','e','f','g','h','i','j'];
   const number = Math.floor(Math.random() * Math.floor(rowList.length));
   let row = rowList[Math.floor(Math.random()*rowList.length)];
   return [row,number];
 }
 
-function setHorizontalPosition (length) {
+function setVerticalPosition(length) {
   let startingPosition = newStartingPoint();
-  // code to fit the ship in horizontal
+  // code to fit the ship in vertically
+  const startingRowIndex = rowList.indexOf(startingPosition[0])
+  let position = [startingPosition];
+  for (let i = 1; i < length; i++) {
+    // find starting position 
+    let rowLetter = rowList[startingRowIndex + i];
+    position.push([rowLetter,startingPosition[1]]);
+  }
+  return position
+}
+
+function setHorizontalPosition(length) {
+  let startingPosition = newStartingPoint();
+  // code to fit the ship in horizontally
     let position = [startingPosition];
     for (let i = 1; i < length; i++) {
       let columnNumber = startingPosition[1] + i;
@@ -33,12 +47,18 @@ function placeShip (length) {
   // pick a direction; up or down
   // if the ship fits set the place
   // if it doesn't fit, get a new starting point
-  const horizontal = true; //() => ((Math.random() < 0.5) >= 0.5) ? true : false;
+  const horizontal = false; //() => ((Math.random() < 0.5) >= 0.5) ? true : false;
 
   if (horizontal === true) {
     let shipPosition = setHorizontalPosition(length);
     while (checkPosition(shipPosition) === false) {
       shipPosition = setHorizontalPosition(length);
+    }
+    return shipPosition;
+  } else {
+    let shipPosition = setVerticalPosition(length);
+    if (checkPosition(shipPosition) === false) { // update to while loop
+      shipPosition = setVerticalPosition(length);
     }
     return shipPosition;
   }
